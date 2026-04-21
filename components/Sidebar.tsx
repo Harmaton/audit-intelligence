34b263b50a243af83ca902d3047e09cf06cb8da7
Harmaton <njagiiharmaton@gmail.com>
2026-04-21 11:40:34 +0300

    minor fix

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import { JSX, useEffect, useState } from "react";
 import {
   IconBuilding,
   IconCheck,
